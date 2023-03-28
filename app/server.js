2fcaab69faf6a697a1d179b0457abc0ed171bc47
Bongisipho <casseteinc@gmail.com>
2023-03-28 22:11:03 +0200

    fixed all errors, now server.js runs properly

--- app/server.js
+++ app/server.js
@@ -9,32 +9,10 @@ let mongoose = require('mongoose');
 let morgan = require('morgan');
 let bodyParser = require('body-parser');
 // let port = 8080;
-import book from './routes/book.js';
 
 const app = express()
 
-import config from '../config/default.json'; //we load the db location from the JSON files
-
 const environment = process.env.NODE_ENV
-
-//db options
-let options = { 
-                server: { socketOptions: { keepAlive: 1, connectTimeoutMS: 30000 } }, 
-                replset: { socketOptions: { keepAlive: 1, connectTimeoutMS : 30000 } } 
-              }; 
-//db connection   
-// const dbHost = process.env.DB_HOST;
-// dbHost.toString();
-const url = "mongodb+srv://Admin:G1w4bV6BwWPUYigT@cluster0.ivy05xp.mongodb.net/?retryWrites=true&w=majority";
-mongoose.connect(url);
-let db = mongoose.connection;
-db.on('error', console.error.bind(console, 'connection error:'));
-
-//don't show the log when it is test
-// if(config.util.getEnv('NODE_ENV') !== 'test') {
-//     //use morgan to log at command line
-//     app.use(morgan('combined')); //'combined' outputs the Apache style LOGs
-// }
 
 if(environment === 'development'){
     dotenv.config({path:process.cwd() + "/.env.development"})
--- build/server.js
+++ build/server.js
@@ -3,8 +3,6 @@
 var _express = _interopRequireDefault(require("express"));
 var _dotenv = _interopRequireDefault(require("dotenv"));
 var _accounts = _interopRequireDefault(require("./routes/accounts"));
-var _book = _interopRequireDefault(require("./routes/book.js"));
-var _default = _interopRequireDefault(require("../config/default.json"));
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
 //--- routes to be imported
 
@@ -16,39 +14,7 @@ var bodyParser = require('body-parser');
 // let port = 8080;
 
 var app = (0, _express["default"])();
-//we load the db location from the JSON files
-
 var environment = process.env.NODE_ENV;
-
-//db options
-var options = {
-  server: {
-    socketOptions: {
-      keepAlive: 1,
-      connectTimeoutMS: 30000
-    }
-  },
-  replset: {
-    socketOptions: {
-      keepAlive: 1,
-      connectTimeoutMS: 30000
-    }
-  }
-};
-//db connection   
-// const dbHost = process.env.DB_HOST;
-// dbHost.toString();
-var url = "mongodb+srv://Admin:G1w4bV6BwWPUYigT@cluster0.ivy05xp.mongodb.net/?retryWrites=true&w=majority";
-mongoose.connect(url);
-var db = mongoose.connection;
-db.on('error', console.error.bind(console, 'connection error:'));
-
-//don't show the log when it is test
-// if(config.util.getEnv('NODE_ENV') !== 'test') {
-//     //use morgan to log at command line
-//     app.use(morgan('combined')); //'combined' outputs the Apache style LOGs
-// }
-
 if (environment === 'development') {
   _dotenv["default"].config({
     path: process.cwd() + "/.env.development"
